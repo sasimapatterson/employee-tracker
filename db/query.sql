51d89994aaf61249d45e4aa0923f838ab099e48e
Sasi Patterson <sweetpea_th@hotmail.com>
2022-09-17 19:33:42 -0400

    got error in terminal that inquirer.prompt is not a function.

--- db/query.sql
+++ db/query.sql
@@ -1,9 +1,19 @@
+-- View all department
 SELECT department.id AS ID, name AS Department
 FROM department;
 
+-- View all roles
 SELECT roles.id AS ID, roles.title AS Title, roles.salary AS Salary, department.name AS Department 
 FROM roles
 LEFT JOIN department ON department.id = roles.department_id;
+
+-- View all employees
+SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name AS Department, 
+roles.salary 
+FROM employee 
+LEFT JOIN roles ON employee.roles_id = roles.id 
+LEFT JOIN department ON roles.department_id = department.id
+LEFT JOIN employee ON employee.manager_id = employee.id;
 
 
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // const { ADDRGETNETWORKPARAMS } = require('dns');
-const inquirer = require('inquirer');
+const inquirer = import('inquirer');
+
 // Import and require mysql12.
 const mysql = require('mysql2');
 // const { allowedNodeEnvironmentFlags } = require('process');
@@ -14,10 +15,15 @@ const connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
+
+    console.log('Glad you are here.');
+    // init();
 });
 
-// Start the application using switch case.
-const init = () => {
+
+
+// // Start the application using switch case.
+function init() {
     inquirer.prompt([
         {
             type: 'list',
@@ -32,56 +38,60 @@ const init = () => {
                 'Add an employee',
                 'Update employee role',
                 'Exit'
-            ]
-        }
+            ],
+        },
     ]).then(response => {
-        switch (response.option) {
-            case 'View all departments':
-                viewAllDepts()
-                break;
-            case 'View all roles':
-                viewAllRoles()
-                break;
-            case 'View all employees':
-                viewAllEmps()
-                break;
-            case 'Add a department':
-                addDept()
-                break;
-            case 'Add a role':
-                addRole()
-                break;
-            case 'Add an Employee':
-                addEmp()
-                break;
-            case 'Update employee role':
-                updateEmpRole()
-                break;
-            case 'Exit':
-                connection.end();
-        }
-    })
-}
-
-// View All Departments
-const viewAllDepts = () => {
-    connection.query(`SELECT department.id AS ID, name AS Department
-    FROM department;`, (err, res) => {
-        if (err) throw err;
-        console.log(res);
-        init();
+        console.log(response);
+        // switch (response.option) {
+        //     case 'View all departments':
+        //         viewAllDepts()
+        //         break;
+        //     case 'View all roles':
+        //         viewAllRoles()
+        //         break;
+        //     case 'View all employees':
+        //         viewAllEmps()
+        //         break;
+        //     case 'Add a department':
+        //         addDept()
+        //         break;
+        //     case 'Add a role':
+        //         addRole()
+        //         break;
+        //     case 'Add an Employee':
+        //         addEmp()
+        //         break;
+        //     case 'Update employee role':
+        //         updateEmpRole()
+        //         break;
+        //     // case 'Exit':
+        //     //     connection.end();
+        // }
     });
-}
+};
 
-// View All Roles
-const viewAllRoles = () => {
-    connection.query(`SELECT roles.id AS ID, roles.title AS Title, roles.salary AS Salary, department.name AS Department 
-    FROM roles
-    LEFT JOIN department 
-    ON department.id = roles.department_id;`,
-        (err, res) => {
-            if (err) throw err;
-            console.log(res);
-            init();
-        });
-}
+init();
+// // View All Departments
+// function viewAllDepts() {
+//     connection.query(`SELECT department.id AS ID, name AS Department
+//     FROM department;`, (err, res) => {
+//         if (err) throw err;
+//         console.log(res);
+//         init();
+//     });
+// }
+
+// // View All Roles
+// function viewAllRoles() {
+//     connection.query(`SELECT roles.id AS ID, roles.title AS Title, roles.salary AS Salary, department.name AS Department 
+//     FROM roles
+//     LEFT JOIN department 
+//     ON department.id = roles.department_id;`,
+//         (err, res) => {
+//             if (err) throw err;
+//             console.log(res);
+//             init();
+//         });
+// }
+
+// init();

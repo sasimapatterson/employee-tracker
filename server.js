75c46ce90e2830d73818d10e6d014aee75304bac
Sasi Patterson <sweetpea_th@hotmail.com>
2022-09-18 14:19:09 -0400

    added addDept, addRole, addEmp functions but haven't test it yet.

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 // const { ADDRGETNETWORKPARAMS } = require('dns');
-const inquirer = require('inquirer');
-
-// Import and require mysql12.
-const mysql = require('mysql2');
 // const { allowedNodeEnvironmentFlags } = require('process');
 
+const inquirer = require('inquirer');
+// Import and require mysql12 to connect to MySQL database.
+const mysql = require('mysql2');
+// Import and require console.table to print MySQL rows to the console.
+const cTable = require('console.table');
 // Connect to database.
 const connection = mysql.createConnection({
     host: "localhost",
@@ -69,7 +70,7 @@ function init() {
         })
 }
 
-init();
+// init();
 // View All Departments
 function viewAllDepts() {
     connection.query(`SELECT department.id AS ID, name AS Department
@@ -108,4 +109,94 @@ function viewAllEmps() {
         });
 }
 
-// init();
+// Add Department. Prompt for name of the department.
+function addDept() {
+    inquirer.prompt({
+        type: 'input',
+        message: 'Please enter the name of the new department',
+        name: 'newDept'
+    }).then(response => {
+        // To capture the value of the department's name that will be added into the database.
+        const newDepartment = response.newDept;
+
+        connection.query(`INSERT INTO department (name) VALUES ("${newDepartment}")`,
+            (err, res) => {
+                if (err) throw err;
+                console.table(res);
+                init();
+            });
+    });
+}
+
+// Add Role. Prompt for employee's title, salary and department for the role.
+function addRole() {
+    inquirer.prompt([ 
+        {
+            type: 'input',
+            message: "Please enter the employee's title",
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'Please enter the salary amount',
+            name: 'salary'
+        },
+        {
+            type: 'input',
+            message: 'Please enter the name of the department',
+            name: 'department'
+        },
+    ]).then (response => {
+        const empNewRole = response.title;
+        const newRoleSalary = response.salary;
+        const newRoleDept = response.department;
+
+        connection.query(`INSERT INTO roles (title, salary, department_id) VALUES ("${empNewRole}", "${newRoleSalary}", "${newRoleDept}")`,
+            (err, res) => {
+                if (err) throw err;
+                console.table(res);
+                init();
+            });
+    });
+}
+
+// Add an employee. Prompt for employee's first and lastname, role and manager.
+function addEmp() {
+    inquirer.prompt ([
+        {
+            type: 'input',
+            message: "Please enter employee's first name",
+            name: 'firstName'
+        },
+        {
+            type: 'input',
+            message: "Please enter employee's last name",
+            name: 'lastName'
+        },
+        {
+            type: 'input',
+            message: "Please enter employee's role",
+            name: 'role'
+        },
+        {
+            type: 'input',
+            message: "Please enter manager's id",
+            name: 'managerId'
+        },
+    ]).then(response => {
+        const empFirstName = response.firstName;
+        const empLastName = response.lastName;
+        const empRole = response.role;
+        const empManager = response.managerId;
+
+        connection.query(`INSERT INTO employee (first_name, last_name, roles_id, manager_id) VALUES ("${empFirstName}", "${empLastName}", "${empRole}", "${empManager}")`,
+            (err, res) => {
+                if (err) throw err;
+                console.table(res);
+                init();
+            });
+    })
+}
+
+
+
